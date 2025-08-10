50ddc879e6691ac8a8ab055617120d26413f5f64
nuskad <ruben.kroman@gmail.com>
2025-08-10 12:46:03 +0200

    new rofi config

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -63,7 +63,7 @@ keys = [
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawn("rofi -show drun"), desc="Spawn a command using a prompt widget"),
+    Key([mod], "r", lazy.spawn("sh -c ~/.config/rofi/scripts/launcher"), desc="Spawn a command using a prompt widget"),
     Key([mod], "p", lazy.spawn("sh -c ~/.config/rofi/scripts/power"), desc='powermenu'),
 
 # C U S T O M
