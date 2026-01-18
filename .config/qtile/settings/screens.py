1fd8ce6f4dffd606c224b4ca7393b9078d1096c6
nuskad <ruben.kroman@gmail.com>
2026-01-18 09:11:11 +0100

    updated config for KDE environment

--- .config/qtile/settings/screens.py
+++
@@ -1,9 +0,0 @@
-from libqtile.config import Screen
-from libqtile import bar
-from .widgets import primary_widgets, secondary_widgets
-
-def status_bar(widgets):
-    return bar.Bar(widgets, 24)
-
-
-screens = [Screen(top=status_bar(primary_widgets)),Screen(top=status_bar(secondary_widgets))]
